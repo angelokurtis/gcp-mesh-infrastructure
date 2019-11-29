ad1fa09914ec95f936d45ce366e37078957a2b1d
Matheus Moraes <matheusfaria.moraes@gmail.com>
2019-11-29 16:07:40 -0300

    updating helm and istio versions and terraform google cloud arguments

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
 output "get_credentials_command" {
-  value = "gcloud container clusters get-credentials ${google_container_cluster.gcloud_cluster.name} --zone ${google_container_cluster.gcloud_cluster.zone} --project ${google_container_cluster.gcloud_cluster.project}"
+  value = "gcloud container clusters get-credentials ${google_container_cluster.gcloud_cluster.name} --zone ${google_container_cluster.gcloud_cluster.location} --project ${google_container_cluster.gcloud_cluster.project}"
+}
+
+output "gcloud_cluster_zone" {
+  value = google_container_cluster.gcloud_cluster.location
 }
 
 output "gcloud_cluster_endpoint" {
--- variables.tf
+++ variables.tf
@@ -16,12 +16,12 @@ variable "cluster_name" {
 
 variable helm_version {
   type = "string"
-  default = "2.15.2"
+  default = "2.14.3"
 }
 
 variable istio_version {
   type = "string"
-  default = "1.3.4"
+  default = "1.4.0"
 }
 
 variable istio_namespace {
